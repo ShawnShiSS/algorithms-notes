c915d5689608a3b975133a7527f2c11cf8b107ef
Shawn Shi <ss.shawnshi@gmail.com>
2021-02-07 20:42:58 -0500

    Two Pointers: add comments for Valid Palindrome

--- src/Algorithms/TwoPointers/TwoPointers.FacingDirection.cs
+++ src/Algorithms/TwoPointers/TwoPointers.FacingDirection.cs
@@ -2,14 +2,15 @@
 
 namespace Algorithms.TwoPointers
 {
-    public class TwoPointers
+    public partial class TwoPointers
     {
         /// <summary>
         ///     LeetCoce 125. Valid Palindrome
         ///     Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+        ///     https://leetcode.com/problems/valid-palindrome/
         /// </summary>
         /// <param name="input"></param>
-        /// <returns></returns>
+        /// <returns>Boolean value indicating whether input is valid palindrome</returns>
         public bool IsPalindrome(string input)
         {
             // edge case
@@ -35,17 +36,30 @@ namespace Algorithms.TwoPointers
                 }
 
                 // comparison ignoring case
-                if (Char.ToLower(input[left]) != Char.ToLower(input[right]))
+                if (!CharsMeetParlindromeRequirement(input[left], input[right]))
                 {
                     return false;
                 }
                 left++;
                 right--;
+                
             }
 
+            // exit when left >= right, indicating string is valid palindrome
             return true;
         }
 
+        /// <summary>
+        ///     Whehter char a and b match for the purpose of palindrome check,
+        ///     E.g., ignoring case
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public bool CharsMeetParlindromeRequirement(char a, char b)
+        {
+            return Char.ToLower(a) == Char.ToLower(b);
+        }
         
 
     }
