53c4407a362b78d470cf2ff0e703791fc354c753
Shawn Shi <ss.shawnshi@gmail.com>
2021-02-11 13:55:56 -0500

    Add binary search template

--- src/Algorithms/BinarySearch/BinarySearch.cs
+++ src/Algorithms/BinarySearch/BinarySearch.cs
@@ -51,7 +51,61 @@ namespace Algorithms.BinarySearch
 
             // target in the right half or not found
             return FindIndexRecursionHelper(nums, mid + 1, end, target);
-            
+        }
+
+        /// <summary>
+        ///     457. Classical Binary Search
+        ///     Find any position of a target number in a sorted array. Return -1 if target does not exist.
+        ///     O(logn) time
+        ///     https://www.lintcode.com/problem/classical-binary-search
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int FindIndexTwoPointers(int[] nums, int target)
+        {
+            // edge case
+            if (nums == null || nums.Length == 0)
+            {
+                return -1;
+            }
+
+            int start = 0;
+            int end = nums.Length - 1;
+            int mid = 0;
+            // NOTE 1: exit when start and end are adjacent
+            while (start + 1 < end)
+            {
+                mid = start + (end - start) / 2; // NOTE 2: avoid overflow
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                
+                if (nums[mid] < target)
+                {
+                    // remove left half
+                    start = mid + 1;
+                }
+
+                if (nums[mid] > target)
+                {
+                    end = mid - 1;
+                }
+            }
+
+            // NOTE : must check both start and end
+            if (nums[start] == target)
+            {
+                return start;
+            }
+
+            if (nums[end] == target)
+            {
+                return end;
+            }
+
+            return -1;
         }
     }
 }
