74fcedfc24ba1b8001c99ad79a461288b5279c64
Shawn Shi <ss.shawnshi@gmail.com>
2021-02-15 12:59:07 -0500

    Two Pointers: Sort Colors and Rainbow sort.

--- src/Algorithms/TwoPointers/TwoPointers.FacingDirection.PartitionType.cs
+++ src/Algorithms/TwoPointers/TwoPointers.FacingDirection.PartitionType.cs
@@ -58,7 +58,128 @@ namespace Algorithms.TwoPointers
         }
 
         /// <summary>
-        ///     Helper method to swap the values by index
+        ///     75. Sort Colors
+        ///     Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+        ///     We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+        ///     https://leetcode.com/problems/sort-colors/
+        /// </summary>
+        /// <param name="nums"></param>
+        public void SortThreeColors(int[] nums)
+        {
+            // edge cases
+            if (nums == null || nums.Length <= 1)
+            {
+                return;
+            }
+
+            // two pointers
+            // left pointer tracking 0s, exclusive
+            // right pointer tracking 2s, exclusive
+            int left = 0;
+            int right = nums.Length - 1;
+            int current = 0;
+            while (current <= right) // NOTE: i<=right, not length
+            {
+                if (nums[current] == 0)
+                {
+                    // swap 0s to left pointer
+                    SwapValues(nums, left, current);
+                    left++;
+                    current++;
+                }
+                else if (nums[current] == 2)
+                {
+                    // swap 2s to right pointer
+                    // BUT DO NOT increment current, as it may be 0 and has to be evaluated again
+                    SwapValues(nums, right, current);
+                    right--;
+                }
+                else
+                {
+                    // skip 1s
+                    current++;
+                }
+            }
+
+        }
+
+        /// <summary>
+        ///     143. Sort Colors II
+        ///     Given an array of n objects with k different colors (numbered from 1 to k), sort them so that objects of the same color are adjacent, with the colors in the order 1, 2, ... k.
+        ///     Solution:
+        ///     - Two Pointers for partitioning
+        ///     - Divide and Conquer to break down the K colors problem
+        ///     https://www.lintcode.com/problem/143/
+        /// </summary>
+        /// <param name="nums"></param>
+        public void SortKColors(int[] nums, int k)
+        {
+            // edge case
+            if (nums == null || nums.Length <= 1)
+            {
+                return;
+            }
+
+            // Rainbow sort: O(nlogk)
+            RainbowSortPartitionHelper(nums, 0, nums.Length - 1, 0, k);
+
+        }
+
+        /// <summary>
+        ///     Partition the array given index range (left and right) so that 
+        ///     - left half of the array is <= mid color, aka, (fromColor+toColor)/2;
+        ///     - right half of the array is > mid color, aka, (fromColor+toColor)/2;
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <param name="fromColor"></param>
+        /// <param name="toColor"></param>
+        private void RainbowSortPartitionHelper(int[] nums, int leftIndex, int rightIndex, int fromColor, int toColor)
+        { 
+            // single color, no sort
+            if (fromColor == toColor)
+            {
+                return;
+            }
+
+            // recursion exit
+            if (leftIndex >= rightIndex)
+            {
+                return;
+            }
+
+            // two pointers template
+            int midColor = fromColor + (toColor - fromColor) / 2;
+            // need to hold on to left and right values for recusive calls
+            int leftPointer = leftIndex; 
+            int rightPointer = rightIndex;
+            while (leftPointer <= rightPointer)
+            {
+                while(leftPointer <= rightPointer && nums[leftPointer] <= midColor) // int division for midColor, hence nums[leftPointer] <= midColor
+                {
+                    leftPointer++;
+                }
+                while (leftPointer <= rightPointer && nums[rightPointer] > midColor)
+                {
+                    rightPointer--;
+                }
+
+                if (leftPointer <= rightPointer)
+                {
+                    SwapValues(nums, leftPointer, rightPointer);
+                    leftPointer++;
+                    rightPointer--;
+                }
+            } // exit when leftPointer>rightPointer
+
+            RainbowSortPartitionHelper(nums, leftIndex, rightPointer, fromColor, midColor);
+
+            RainbowSortPartitionHelper(nums, leftPointer, rightIndex, midColor + 1, toColor);
+        }
+
+        /// <summary>
+        ///     Swap two values in an array by index.
         /// </summary>
         /// <param name="nums"></param>
         /// <param name="index1"></param>
