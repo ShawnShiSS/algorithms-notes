1263295d2fc1045d796fc5b5be0a51ff3705cc92
Shawn Shi <ss.shawnshi@gmail.com>
2021-02-20 21:51:38 -0500

    Bug fix.

---
+++ src/Algorithms/DepthFirstSearch/BSTIterator.cs
@@ -0,0 +1,56 @@
+﻿using Algorithms.Tree;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.DepthFirstSearch
+{
+    /// <summary>
+    ///     173. Binary Search Tree Iterator
+    ///     Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST)
+    ///     https://leetcode.com/problems/binary-search-tree-iterator/
+    /// </summary>
+    public class BSTIterator
+    {
+        /// <summary>
+        ///     Stack that holds all tree nodes whose right subtree has not been iterated yet.
+        /// </summary>
+        private Stack<TreeNode> _stack;
+
+        public BSTIterator(TreeNode root)
+        {
+            this._stack = new Stack<TreeNode>();
+            FillTowardsMostLeft(root);
+        }
+
+        public int Next()
+        {
+            TreeNode current = _stack.Pop();
+            if (current.RightChild != null)
+            {
+                FillTowardsMostLeft(current.RightChild);
+            }
+
+            return current.Value;
+        }
+
+        /// <summary>
+        ///     Whether the BST has next node to iterate 
+        /// </summary>
+        /// <returns></returns>
+        public bool HasNext()
+        {
+            return _stack.Count > 0;
+        }
+
+        private void FillTowardsMostLeft(TreeNode root)
+        {
+            // NOTE 1: add node and all left children to the stack
+            while (root != null)
+            {
+                _stack.Push(root);
+                root = root.LeftChild;
+            }
+        }
+    }
+}
--- src/Algorithms/DepthFirstSearch/DepthFirstSearch.BST.cs
+++ src/Algorithms/DepthFirstSearch/DepthFirstSearch.BST.cs
@@ -5,55 +5,5 @@ using Algorithms.Tree;
 
 namespace Algorithms.DepthFirstSearch
 {
-    public partial class DepthFirstSearch
-    {
-        /// <summary>
-        ///     173. Binary Search Tree Iterator
-        ///     Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST)
-        ///     https://leetcode.com/problems/binary-search-tree-iterator/
-        /// </summary>
-        public class BSTIterator
-        {
-            /// <summary>
-            ///     Stack that holds all tree nodes whose right subtree has not been iterated yet.
-            /// </summary>
-            private Stack<TreeNode> _stack;
-
-            public BSTIterator(TreeNode root)
-            {
-                this._stack =  = new Stack<TreeNode>();
-                FillTowardsMostLeft(root);
-            }
-            
-            public int Next()
-            {
-                TreeNode current = _stack.Pop();
-                if (current.Right != null)
-                {
-                    FillTowardsMostLeft(current.Right);
-                }
-
-                return current;
-            }
-
-            /// <summary>
-            ///     Whether the BST has next node to iterate 
-            /// </summary>
-            /// <returns></returns>
-            public bool HasNext()
-            {
-                return stack.Count > 0;
-            }
-
-            private void FillTowardsMostLeft(TreeNode root)
-            {
-                // NOTE 1: add node and all left children to the stack
-                while (root != null)
-                {
-                    stack.Push(root);
-                    root = root.left;
-                }
-            }
-        }
-    }
+    
 }
