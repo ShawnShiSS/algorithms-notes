1f6a7d927657ce8ab38e6754196ee557117a17b6
Shawn Shi <ss.shawnshi@gmail.com>
2021-02-20 21:36:31 -0500

    TreeNode: specify constructor.

--- src/Algorithms/Tree/TreeNode.cs
+++ src/Algorithms/Tree/TreeNode.cs
@@ -9,12 +9,26 @@ namespace Algorithms.Tree
     /// </summary>
     public class TreeNode
     {
+        public TreeNode(int value, TreeNode left, TreeNode right)
+        {
+            this.Value = value;
+            this.LeftChild = left;
+            this.RightChild = right;
+        }
+
         /// <summary>
         ///     Node value
         /// </summary>
         public int Value { get; set; }
 
+        /// <summary>
+        ///     Left Child
+        /// </summary>
         public TreeNode LeftChild { get; set; }
+
+        /// <summary>
+        ///     Right child
+        /// </summary>
         public TreeNode RightChild { get; set; }
     }
 }
