fc9f957d1a89aa451d7753c3524637da707e82ef
Shawn Shi <ss.shawnshi@gmail.com>
2021-02-20 20:33:51 -0500

    Tree: define tree node

---
+++ src/Algorithms/Tree/TreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tree
+{
+    /// <summary>
+    ///     Tree Node
+    /// </summary>
+    public class TreeNode
+    {
+        /// <summary>
+        ///     Node value
+        /// </summary>
+        public int Value { get; set; }
+
+        public TreeNode LeftChild { get; set; }
+        public TreeNode RightChild { get; set; }
+    }
+}
