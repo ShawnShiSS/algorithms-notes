a1367c9fa6410db73760c94cd37dda9296793e28
Shawn Shi <ss.shawnshi@gmail.com>
2021-02-22 22:39:06 -0500

    Graph: add definition for directed graph

---
+++ src/Algorithms/Graph/DirectedGraph.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Graph
+{
+    /// <summary>
+    ///     Definition for Directed Graph
+    /// </summary>
+    public class DirectedGraph
+    {
+        public DirectedGraph(int label)
+        {
+            this.Label = label;
+            this.Neighbours = new List<DirectedGraph>();
+        }
+        public int Label { get; set; }
+        public List<DirectedGraph> Neighbours { get; set; }
+    }
+}
