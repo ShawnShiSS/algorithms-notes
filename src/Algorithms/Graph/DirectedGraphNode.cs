0fdd2218bfc308f0b30b0046dd2dd435fc655b13
Shawn Shi <ss.shawnshi@gmail.com>
2021-02-22 22:57:16 -0500

    Graph definitions

---
+++ src/Algorithms/Graph/DirectedGraphNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Graph
+{
+    /// <summary>
+    ///     Definition for Directed Graph Node
+    /// </summary>
+    public class DirectedGraphNode
+    {
+        public DirectedGraphNode(int label)
+        {
+            this.Label = label;
+            this.Neighbours = new List<DirectedGraphNode>();
+        }
+        public int Label { get; set; }
+        public List<DirectedGraphNode> Neighbours { get; set; }
+
+    }
+}
