87b8bf527af9671ae77ae4c46c64b55f51b5d7aa
Shawn Shi <ss.shawnshi@gmail.com>
2021-03-01 22:04:24 -0500

    DFS: subsets with duplicted elements

--- src/Algorithms/DepthFirstSearch/DepthFirstSearch.NonTreeTemplate.cs
+++ src/Algorithms/DepthFirstSearch/DepthFirstSearch.NonTreeTemplate.cs
@@ -14,7 +14,12 @@ namespace Algorithms.DepthFirstSearch
         ///     Given an integer array nums of unique elements, return all possible subsets (the power set).
         ///     The solution set must not contain duplicate subsets.Return the solution in any order.
         ///     https://leetcode.com/problems/subsets/
-        ///     Solution: DFS template
+        ///     Solution: DFS template without duplicates
+        ///     90. Subsets II
+        ///     Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+        ///     The solution set must not contain duplicate subsets.Return the solution in any order.
+        ///     https://leetcode.com/problems/subsets-ii/
+        ///     Solution: DFS template with duplicates
         /// </summary>
         /// <param name="nums"></param>
         /// <returns></returns>
@@ -33,7 +38,7 @@ namespace Algorithms.DepthFirstSearch
             Array.Sort(nums);
 
             // DFS with recursion
-            DfsCombinationTemplateHelper(nums, 0, new List<int>(), result);
+            DfsCombinationWithoutDuplicatesTemplateHelper(nums, 0, new List<int>(), result);
 
             return result;
         }
@@ -46,7 +51,7 @@ namespace Algorithms.DepthFirstSearch
         /// <param name="startIndex"></param>
         /// <param name="subset"></param>
         /// <param name="result"></param>
-        private void DfsCombinationTemplateHelper(int[] nums,
+        private void DfsCombinationWithoutDuplicatesTemplateHelper(int[] nums,
                                                   int startIndex,
                                                   IList<int> subset,
                                                   IList<IList<int>> result)
@@ -61,7 +66,47 @@ namespace Algorithms.DepthFirstSearch
                 // add the current element to the subset
                 subset.Add(nums[i]);
                 // search all the subsets that start with the subset
-                DfsCombinationTemplateHelper(nums, i + 1, subset, result);
+                DfsCombinationWithoutDuplicatesTemplateHelper(nums, i + 1, subset, result);
+
+                // backtracking, must remove the current element 
+                subset.RemoveAt(subset.Count - 1);
+            }
+        }
+
+        /// <summary>
+        ///     Traverse an array to find all the subsets that start with subset,
+        ///     and add the subsets to result in place.
+        ///     Note the array, nums, may have duplicated elements.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="subset"></param>
+        /// <param name="result"></param>
+        private void DfsCombinationWithDuplicatesTemplateHelper(int[] nums,
+                                                  int startIndex,
+                                                  IList<int> subset,
+                                                  IList<IList<int>> result)
+        {
+            // Recursion exit
+            // Deep copy the subset into result
+            result.Add(new List<int>(subset));
+
+            // Recursion 
+            for (int i = startIndex; i < nums.Length; i++)
+            {
+                // Must remove duplicates
+                if (i > 0 && // Out of bound check
+                    nums[i] == nums[i-1] && // same number already added for the current recursion
+                    i > startIndex // not the first time seeing this element
+                    )
+                {
+                    continue;
+                }
+
+                // add the current element to the subset
+                subset.Add(nums[i]);
+                // search all the subsets that start with the subset
+                DfsCombinationWithDuplicatesTemplateHelper(nums, i + 1, subset, result);
 
                 // backtracking, must remove the current element 
                 subset.RemoveAt(subset.Count - 1);
