f080678a037780600ce47b0643e9a59c9d5c1ee9
Shawn Shi <ss.shawnshi@gmail.com>
2021-03-13 22:58:07 -0500

    Dynamic Programming: backpack 01 type

---
+++ src/Algorithms/DynamicProgramming/DynamicProgramming.Backpack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.DynamicProgramming
+{
+    /// <summary>
+    ///     Dynamic programming for backpack-type problems
+    /// </summary>
+    public partial class DynamicProgramming
+    {
+        /// <summary>
+        ///     ///     92. Backpack
+        ///     Given n items, each item has size items[i], an integer m denotes the size of a backpack. How full you can fill this backpack?
+        ///     https://www.lintcode.com/problem/backpack/
+        ///     Solution: Dynamic programming for backpack
+        /// </summary>
+        /// <param name="backpackSize"></param>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public int Backpack(int backpackSize, int[] items)
+        {
+            int itemCount = items.Length;
+
+            // DP state: dp[i][j] whether the first i items can create a sum = j
+            // Note: need to also track 0 item can create sum = 0, thun itemCount+1 below
+            bool[][] dp = new bool[itemCount + 1][];
+            for (int i = 0; i < dp.Length; i++)
+            {
+                // Need to also track sum = 0, thus backpackSize + 1
+                dp[i] = new bool[backpackSize + 1];
+            }
+
+            // DP initialization: first 0 items to create a sum = 0
+            dp[0][0] = true;
+
+            // DP function
+            // first x items can make a sum
+            // , or first i items can make a sum = backpackSize - A[j]
+            // start from i = 1, as 0 item can not make a sum > 0
+            for(int itemIndex = 1; itemIndex < itemCount + 1; itemIndex++)
+            {
+                // first x item of course can make a sum = 0, just do not pick it.
+                dp[itemIndex][0] = true;
+                for (int sum = 1; sum <= backpackSize; sum++)
+                {
+                    
+                    if (sum >= items[itemIndex - 1])
+                    {
+                        // May pick the current item
+                        dp[itemIndex][sum] = dp[itemIndex - 1][sum] || 
+                                             dp[itemIndex - 1][sum - items[itemIndex - 1]];
+                    }
+                    else
+                    {
+                        // Can not pick the current item
+                        dp[itemIndex][sum] = dp[itemIndex - 1][sum];
+                    }
+                }
+            }
+
+            // DP Answer
+            for (int sum = backpackSize; sum >=0; sum--)
+            {
+                if (dp[itemCount][sum])
+                {
+                    return sum;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
