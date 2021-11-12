abe710a2caa0174af038d46824ec32a8a43b6e9e
Shawn Shi <ss.shawnshi@gmail.com>
2021-11-12 07:25:22 -0500

    Add test console project

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Algorithms.Stack.Parenthesis p= new Algorithms.Stack.Parenthesis();
+
+            var output = p.MinRemoveToMakeValid("lee(t(c)o)de)");
+
+            Console.WriteLine(output);
+        }
+    }
+}
