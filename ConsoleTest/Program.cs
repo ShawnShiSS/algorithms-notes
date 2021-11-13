477ff2c319a41fbe960fa9e1ecdc3f9889737e4b
Shawn Shi <ss.shawnshi@gmail.com>
2021-11-13 11:08:21 -0500

    BFS - zigzag level order

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Algorithms.Tree;
+using System;
 
 namespace ConsoleTest
 {
@@ -7,11 +8,21 @@ namespace ConsoleTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Algorithms.Stack.Parenthesis p= new Algorithms.Stack.Parenthesis();
+            TreeNode root = new TreeNode(3, null, null);
+            TreeNode node9 = new TreeNode(9, null, null);
+            TreeNode node20 = new TreeNode(20, null, null);
+            TreeNode node15 = new TreeNode(15, null, null);
+            TreeNode node7 = new TreeNode(7, null, null);
 
-            var output = p.MinRemoveToMakeValid("lee(t(c)o)de)");
+            node20.LeftChild = node15;
+            node20.RightChild = node7;
+            root.LeftChild = node9;
+            root.RightChild = node20;
 
-            Console.WriteLine(output);
+            var bfs = new Algorithms.BreadthFirstSearch.BreadthFirstSearch();
+            var result = bfs.ZigzagLevelOrder(root);
+
+            Console.WriteLine("goodbye");
         }
     }
 }
--- src/Algorithms/BreadthFirstSearch/BreadthFirstSearch.LevelOrderTraversal.cs
+++ src/Algorithms/BreadthFirstSearch/BreadthFirstSearch.LevelOrderTraversal.cs
@@ -1,10 +1,79 @@
-﻿using System;
+﻿using Algorithms.Tree;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Algorithms.BreadthFirstSearch
 {
     public partial class BreadthFirstSearch
     {
+        /// <summary>
+        ///     103. Binary Tree Zigzag Level Order Traversal
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            // Approach
+            // BFS level order traversal, plus a flag to track direction.
+
+            IList<IList<int>> result = new List<IList<int>>();
+
+            // Corner cases
+            if (root == null)
+            {
+                return result;
+            }
+
+            // BFS search using a queue
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            bool goRight = false;
+
+            while (queue.Count > 0)
+            {
+                // Hold on to the current layer size
+                int layerSize = queue.Count;
+
+                // Search this layer
+                IList<int> currentLayer = new List<int>();
+                for (int i = 0; i < layerSize; i++)
+                {
+                    TreeNode currentNode = queue.Dequeue();
+                    currentLayer.Add(currentNode.Value);
+
+                    // Push child nodes to the queue
+                    if (currentNode.LeftChild != null)
+                    {
+                        queue.Enqueue(currentNode.LeftChild);
+                    }
+                    if (currentNode.RightChild != null)
+                    {
+                        queue.Enqueue(currentNode.RightChild);
+                    }
+                }
+
+                // Honor the direction flag
+                if (!goRight)
+                {
+                    ReverseList(currentLayer);
+                }
+
+                // Deep copy current layer into the result
+                result.Add(new List<int>(currentLayer));
+
+                // Reset direction flag
+                goRight = !goRight;
+            }
+
+            return result;
+        }
+
+        private void ReverseList(IList<int> list)
+        {
+            List<int> concreteList = (List<int>)list;
+            concreteList.Reverse();
+        }
     }
 }
